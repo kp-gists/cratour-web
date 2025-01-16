31cb137ea2f8640859fae0e4d0fc9efc45992d0e
Sokol Paja <kolpaja28@gmail.com>
2025-01-16 20:57:28 +0100

    --legacy-peer-deps

--- components/ui/DatePicker.tsx
+++ components/ui/DatePicker.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import * as React from 'react';
-import { format } from 'date-fns';
 import { Calendar as CalendarIcon } from 'lucide-react';
 
 import { cn } from '@/lib/utils';
@@ -10,12 +9,20 @@ import { Calendar } from '@/components/ui/calendar';
 import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/popover';
 
 export function DatePickerDemo({ selected, onChange }: { selected?: Date; onChange: any }) {
+	const formatDate = (date: Date) => {
+		return date.toLocaleDateString(undefined, {
+			weekday: 'long', // Example: "Thursday"
+			year: 'numeric', // Example: "2025"
+			month: 'long', // Example: "January"
+			day: 'numeric', // Example: "16"
+		});
+	};
 	return (
 		<Popover>
 			<PopoverTrigger asChild>
 				<Button variant={'outline'} className={cn('w-[280px] justify-start text-left font-normal', !selected && 'text-muted-foreground')}>
 					<CalendarIcon className='mr-2 h-4 w-4' />
-					{selected ? format(selected, 'PPP') : <span>Pick a date</span>}
+					{selected ? formatDate(selected) : <span>Pick a date</span>}
 				</Button>
 			</PopoverTrigger>
 			<PopoverContent className='w-auto p-0'>
