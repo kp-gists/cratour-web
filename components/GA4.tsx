96e97ed8d9159e4b1f96e2d84d680e3be1779382
Sokol Paja <kolpaja28@gmail.com>
2025-03-06 18:25:58 +0100

    add consent

--- components/GA4.tsx
+++ components/GA4.tsx
@@ -12,6 +12,13 @@ declare global {
 export default function GoogleAnalytics() {
 	return (
 		<>
+			<Script id='' type='text/javascript'>
+				{`var _iub = _iub || [];
+_iub.csConfiguration = {"siteId":3953466,"cookiePolicyId":57630008,"lang":"en","storage":{"useSiteId":true}};`}
+			</Script>
+			<Script type='text/javascript' src='https://cs.iubenda.com/autoblocking/3953466.js'></Script>
+			<Script type='text/javascript' src='//cdn.iubenda.com/cs/gpp/stub.js'></Script>
+			<Script type='text/javascript' src='//cdn.iubenda.com/cs/iubenda_cs.js' charSet='UTF-8' async></Script>
 			<Script src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`} strategy='afterInteractive' />
 			<Script strategy='afterInteractive' id='google-analytics'>{`
 				  window.dataLayer = window.dataLayer || [];
